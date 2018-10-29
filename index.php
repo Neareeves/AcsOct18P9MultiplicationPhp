fed4e2e9b2fc0316419cee30468fff34fc172dde
Neareeves <alethesj@gmail.com>
2018-10-29 11:27:40 +0100

    trame vide

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="stylesaleth.css">
+	<title>Outil de révision des tables de multiplication</title>
+</head>
+<body>
+<?php
+
+
+
+
+?>
+</body>
+</html>
